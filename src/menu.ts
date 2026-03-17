66878e56f354125902be641ec8711e68dcb6432a
cristianreyes0209-droid <cristian.reyes0209@gmail.com>
2026-03-17 17:27:07 -0500

    Update menu.ts

--- src/menu.ts
+++ src/menu.ts
@@ -3,11 +3,13 @@ export const menu = {
     {
       id: "paris",
       nombre: "París",
+       precio: 29500,
       aliases: ["paris", "parís", "crepe paris"]
     },
     {
       id: "desgranada_mixta",
       nombre: "Desgranada mixta",
+       precio: 27500,
       aliases: ["desgranada mixta", "mixta", "desgranada"]
     },
     {
@@ -19,41 +21,49 @@ export const menu = {
     {
       id: "hawaiana",
       nombre: "Hawaiana",
+       precio: 14900,
       aliases: ["hawaiana"]
     },
     {
       id: "pollo",
       nombre: "Pollo",
+       precio: 14500,
       aliases: ["pollo", "crepe de pollo"]
     },
     {
       id: "pollo_pina",
       nombre: "Pollo y piña",
+       precio: 15500,
       aliases: ["pollo y piña", "pollo pina", "pollo piña"]
     },
     {
       id: "pollo_champinones",
       nombre: "Pollo champiñones",
+       precio: 16500,
       aliases: ["pollo champiñones", "pollo champinones"]
     },
     {
       id: "carne",
       nombre: "Carne",
+       precio: 17000,
       aliases: ["carne", "crepe de carne"]
     },
     {
       id: "nutella",
       nombre: "Nutella",
+       precio: 11500,
       aliases: ["nutella"]
     },
     {
       id: "tropinutella",
       nombre: "Tropinutella",
+       precio: 19900,
       aliases: ["tropinutella"]
     },
     {
       id: "tropical",
       nombre: "Tropical",
+       precio: 16900,
       aliases: ["tropical"]
     }
   ]
