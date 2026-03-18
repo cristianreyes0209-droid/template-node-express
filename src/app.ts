e4b0544d6c6ddcaee4245b5e2d8720b623548020
cristianreyes0209-droid <cristian.reyes0209@gmail.com>
2026-03-17 22:36:46 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -232,7 +232,7 @@ if (currentOrder?.step === "esperando_nombre") {
     replyMessage = "Por favor dime si tu pedido es para domicilio o para recoger.";
   }
 
- else if (currentOrder?.step === "esperando_direccion") {
+} else if (currentOrder?.step === "esperando_direccion") {
     updateOrderAddress(phone, text);
   const order = getOrder(phone)!;
   const totals = calculateTotal(order);
