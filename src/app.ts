4f41253fac83251b25c0e34f4b224c37544fb40f
cristianreyes0209-droid <cristian.reyes0209@gmail.com>
2026-03-21 22:32:30 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -641,7 +641,7 @@ const resumen = order.items
 
     console.log("========== ORDEN FINAL JSON ==========");
     console.log(JSON.stringify(orderJSON, null, 2));
-      const order = getOrder(phone)!;
+     
 const totals = calculateTotal(order);
 
 const resumen = order.items
