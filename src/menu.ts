caf17b16a69208a1b6759a1fb88e4da89ea27113
cristianreyes0209-droid <cristian.reyes0209@gmail.com>
2026-03-21 22:54:03 -0500

    Update menu.ts

--- src/menu.ts
+++ src/menu.ts
@@ -175,7 +175,275 @@ export const menu = {
     "crepostre", "crepostres",
     "postre", "postres",
     "crepe postre", "crepes postre"
+    },
+{
+ 
+  id: "jugo_mango_agua",
+  nombre: "Jugo de mango en agua",
+  precio: 9900,
+  aliases: [
+    "jugo de mango en agua",
+    "mango en agua",
+    "jugo mango en agua"
+  ]
+},
+{
+  id: "jugo_mango_leche",
+  nombre: "Jugo de mango en leche",
+  precio: 11500,
+  aliases: [
+    "jugo de mango en leche",
+    "mango en leche",
+    "jugo mango en leche"
+  ]
+},
+{
+  id: "jugo_maracuya_agua",
+  nombre: "Jugo de maracuyá en agua",
+  precio: 9900,
+  aliases: [
+    "jugo de maracuya en agua",
+    "jugo de maracuyá en agua",
+    "maracuya en agua",
+    "maracuyá en agua"
+  ]
+},
+{
+  id: "jugo_maracuya_leche",
+  nombre: "Jugo de maracuyá en leche",
+  precio: 11500,
+  aliases: [
+    "jugo de maracuya en leche",
+    "jugo de maracuyá en leche",
+    "maracuya en leche",
+    "maracuyá en leche"
+  ]
+},
+{
+  id: "jugo_mora_agua",
+  nombre: "Jugo de mora en agua",
+  precio: 9900,
+  aliases: [
+    "jugo de mora en agua",
+    "mora en agua",
+    "jugo mora en agua"
+  ]
+},
+{
+  id: "jugo_mora_leche",
+  nombre: "Jugo de mora en leche",
+  precio: 11500,
+  aliases: [
+    "jugo de mora en leche",
+    "mora en leche",
+    "jugo mora en leche"
+  ]
+},
+{
+  id: "jugo_fresa_agua",
+  nombre: "Jugo de fresa en agua",
+  precio: 9900,
+  aliases: [
+    "jugo de fresa en agua",
+    "fresa en agua",
+    "jugo fresa en agua"
+  ]
+},
+{
+  id: "jugo_fresa_leche",
+  nombre: "Jugo de fresa en leche",
+  precio: 11500,
+  aliases: [
+    "jugo de fresa en leche",
+    "fresa en leche",
+    "jugo fresa en leche"
+  ]
+},
+{
+  id: "jugo_guanabana_agua",
+  nombre: "Jugo de guanábana en agua",
+  precio: 9900,
+  aliases: [
+    "jugo de guanabana en agua",
+    "jugo de guanábana en agua",
+    "guanabana en agua",
+    "guanábana en agua"
+  ]
+},
+{
+  id: "jugo_guanabana_leche",
+  nombre: "Jugo de guanábana en leche",
+  precio: 11500,
+  aliases: [
+    "jugo de guanabana en leche",
+    "jugo de guanábana en leche",
+    "guanabana en leche",
+    "guanábana en leche"
+  ]
+},
+{
+  id: "limonada_natural",
+  nombre: "Limonada natural",
+  precio: 5000,
+  aliases: [
+    "limonada natural",
+    "una limonada natural"
+  ]
+},
+{
+  id: "limonada_coco",
+  nombre: "Limonada de coco",
+  precio: 10500,
+  aliases: [
+    "limonada de coco",
+    "una limonada de coco"
+  ]
+},
+{
+  id: "limonada_cerezada",
+  nombre: "Limonada cerezada",
+  precio: 12500,
+  aliases: [
+    "limonada cerezada",
+    "una limonada cerezada"
+  ]
+},
+{
+  id: "malteada_vainilla_brownie",
+  nombre: "Malteada vainilla brownie",
+  precio: 17900,
+  aliases: [
+    "malteada vainilla brownie",
+    "vainilla brownie"
+  ]
+},
+{
+  id: "malteada_vainilla",
+  nombre: "Malteada de vainilla",
+  precio: 17900,
+  aliases: [
+    "malteada de vainilla",
+    "malteada vainilla"
+  ]
+},
+{
+  id: "malteada_fresa",
+  nombre: "Malteada de fresa",
+  precio: 17900,
+  aliases: [
+    "malteada de fresa",
+    "malteada fresa"
+  ]
+},
+{
+  id: "agua_embotellada",
+  nombre: "Agua",
+  precio: 3900,
+  aliases: [
+    "agua",
+    "agua embotellada",
+    "una agua"
+  ]
+},
+{
+  id: "coca_cola",
+  nombre: "Coca Cola",
+  precio: 5900,
+  aliases: [
+    "coca cola",
+    "cocacola",
+    "una coca cola"
+  ]
+},
+{
+  id: "coca_cola_zero",
+  nombre: "Coca Cola Zero",
+  precio: 5900,
+  aliases: [
+    "coca cola zero",
+    "cocacola zero",
+    "una coca cola zero"
+  ]
+},
+{
+  id: "jugo_hit",
+  nombre: "Jugo Hit",
+  precio: 5900,
+  aliases: [
+    "jugo hit",
+    "hit",
+    "un hit"
+  ]
+},
+{
+  id: "gaseosa_postobon",
+  nombre: "Gaseosa Postobón",
+  precio: 5900,
+  aliases: [
+    "gaseosa postobon",
+    "gaseosa postobón",
+    "postobon",
+    "postobón"
+  ]
+},
+{
+  id: "mr_tea_pet_500",
+  nombre: "Mr Tea Pet 500",
+  precio: 5900,
+  aliases: [
+    "mr tea",
+    "mr tea pet 500",
+    "mr tea 500"
+  ]
+},
+{
+  id: "bretana",
+  nombre: "Bretaña",
+  precio: 4900,
+  aliases: [
+    "bretaña",
+    "bretana",
+    "una bretaña"
+  ]
+},
+{
+  id: "h2o_pet_400",
+  nombre: "H2O Pet 400",
+  precio: 5900,
+  aliases: [
+    "h2o",
+    "h2o pet 400",
+    "un h2o"
+  ]
+},
+{
+  id: "saborizada_pet_400",
+  nombre: "Saborizada Pet 400",
+  precio: 5900,
+  aliases: [
+    "saborizada",
+    "saborizada pet 400",
+    "una saborizada"
+  ]
+},
+{
+  id: "te_hatsu",
+  nombre: "Té Hatsu",
+  precio: 9900,
+  aliases: [
+    "te hatsu",
+    "té hatsu",
+    "hatsu"
+  ]
+},
+{
+    id: "club_colombia",
+    nombre: "Club Colombia",
+    precio: 8900,
+    aliases: [
+      "club colombia",
+      "una club colombia"
     ]
-}
+  }
   ]
 };
