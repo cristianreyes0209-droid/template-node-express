1c101d0d9455bdb5101e7c46eaa791e0d3a5e6b3
cristianreyes0209-droid <cristian.reyes0209@gmail.com>
2026-03-28 17:27:28 -0500

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,1 +1,16 @@
+import { Pool } from "pg";
 
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+pool.connect()
+  .then(() => {
+    console.log("✅ Conectado a PostgreSQL");
+  })
+  .catch((err) => {
+    console.error("❌ Error conectando a PostgreSQL:", err);
+  });
